15be7e227ccfb20e4cc603d896742ed930672380
Brent <brentcarey1@outlook.com>
2020-04-29 00:42:26 +0000

    Finished 5 and 6

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,12 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+puts Store.sum(:annual_revenue)
+
+puts Store.average(:annual_revenue)
+
+@rich = Store.where(annual_revenue:1000000..Float::INFINITY)
+
+@rich.each do |store|
+    puts store.name
+end
--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,9 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store2.employees.create(first_name: "Brent", last_name: "Carey", hourly_rate: 1000)
+@store4.employees.create(first_name: "Izzy", last_name: "Spears", hourly_rate: 2000)
+@store5.employees.create(first_name: "Donald", last_name: "Trump", hourly_rate: 3)
+@store6.employees.create(first_name: "Aubrey", last_name: "Graham", hourly_rate: 100000)
+
